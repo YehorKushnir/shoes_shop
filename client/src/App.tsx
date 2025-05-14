5ff0c29845c4c453e0b35e935b6080d840e951d3
Yehor Kushnir <yehor.kushnir@gmail.com>
2025-05-14 19:32:57 +0300

    fix navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,6 @@ import ProtectedRoute from '@/components/protected-route.tsx'
 import {protectedRoutes, publicRoutes} from '@/utils/routes.tsx'
 import PublicRoute from '@/components/public-route.tsx'
 import {Navbar1} from '@/components/navbar1.tsx'
-import {Skeleton} from '@/components/ui/skeleton.tsx'
 
 const hideNavbarPaths = ['/login', '/register']
 const onlyAdminPages = ['/admin-panel']
@@ -20,20 +19,17 @@ function App() {
     const location = useLocation()
     const isNavbarVisible = !hideNavbarPaths.includes(location.pathname)
 
-    const loading = useStore(state => state.loading)
     const user = useStore(state => state.user)
     const refresh = useStore(state => state.refresh)
 
     useEffect(() => {
         void refresh()
     }, [])
-    console.log(location, loading)
 
     return (
         <>
-            {isNavbarVisible && (loading
-                ? <Skeleton className={'w-[100%-32px] h-10 m-4'}/>
-                : <Navbar1 menu={user?.role === "admin" ? menu : menu.filter(item => !onlyAdminPages.includes(item.url))} />
+            {isNavbarVisible && (
+                <Navbar1 menu={user?.role === "admin" ? menu : menu.filter(item => !onlyAdminPages.includes(item.url))} />
             )}
             <Routes>
                 {publicRoutes.map(({path, element, redirectIfAuth}) => (
